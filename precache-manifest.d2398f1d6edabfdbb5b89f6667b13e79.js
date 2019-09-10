4d1e6d9dc9d99d35491f1ddb3e59da9f424bb9be
Kushal <khachnek123@gmail.com>
2019-09-10 22:45:56 +0530

    Updates

---
+++ precache-manifest.d2398f1d6edabfdbb5b89f6667b13e79.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e93d1e155cf65e40e5ee9aedf0e447c8",
+    "url": "/monsters-search/index.html"
+  },
+  {
+    "revision": "c82d44c60942a9c9c106",
+    "url": "/monsters-search/static/css/main.4e701262.chunk.css"
+  },
+  {
+    "revision": "46012db1d69cedf8d1ab",
+    "url": "/monsters-search/static/js/2.3d8af1b9.chunk.js"
+  },
+  {
+    "revision": "c82d44c60942a9c9c106",
+    "url": "/monsters-search/static/js/main.52ec39e0.chunk.js"
+  },
+  {
+    "revision": "011cea44b56e916ca61d",
+    "url": "/monsters-search/static/js/runtime~main.46124b2e.js"
+  }
+]);
